d1bbcb35a57f0494cec2ff77e1efd9d90d2f7e34
desan <desanctisisabella95@gmail.com>
2025-04-25 15:03:39 +0200

    commit esercizio 3 test

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -6,24 +6,24 @@ import java.util.Locale;
 
 
 public class Main {
-    public static void main(String[] args) {
-        // Creazione dell'oggetto OffsetDateTime dalla stringa
-        OffsetDateTime dateTime = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+public static void main(String[] args) {
+    // Creazione dell'oggetto OffsetDateTime dalla stringa
+    OffsetDateTime dateTime = OffsetDateTime.parse("2023-03-01T13:00:00Z");
 
 
-        // Formattazione della data in vari stili
-        String fullFormat = formatDateTime(dateTime, DateTimeFormatter.ofPattern("EEEE, d MMMM yyyy HH:mm:ss O", Locale.ITALIAN));
-        String mediumFormat = formatDateTime(dateTime, DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss", Locale.ITALIAN));
-        String shortFormat = formatDateTime(dateTime, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm", Locale.ITALIAN));
+    // Formattazione della data in vari stili
+    //String fullFormat = formatDateTime(dateTime, DateTimeFormatter.ofPattern("EEEE, d MMMM yyyy HH:mm:ss O", Locale.ITALIAN));
+    //String mediumFormat = formatDateTime(dateTime, DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss", Locale.ITALIAN));
+    //String shortFormat = formatDateTime(dateTime, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm", Locale.ITALIAN));
+    String longFormat = formatDateTime(dateTime, DateTimeFormatter.ofPattern("d MMMM yyyy ", Locale.ITALIAN)); //funzione che restituisce : 1 marzo 2023
+    // Stampa delle versioni formattate
+    //System.out.println("Formato FULL: " + fullFormat);
+   // System.out.println("Formato MEDIUM: " + mediumFormat);
+    System.out.println("Formato LONG: " + longFormat);
+}
 
-        // Stampa delle versioni formattate
-        System.out.println("Formato FULL: " + fullFormat);
-        System.out.println("Formato MEDIUM: " + mediumFormat);
-        System.out.println("Formato SHORT: " + shortFormat);
-    }
-
-    // Metodo per formattare la data
-    public static String formatDateTime(OffsetDateTime dateTime, DateTimeFormatter formatter) {
-        return dateTime.format(formatter);
-    }
+// Metodo per formattare la data
+public static String formatDateTime(OffsetDateTime dateTime, DateTimeFormatter formatter) {
+    return dateTime.format(formatter);
+}
 }
